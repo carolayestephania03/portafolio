f738a8037e400ce2ce16a2f14e779711d9ac2742
CarolayCante <carolaycante@gmail.com>
2024-10-03 22:08:58 -0600

    Primer Commit

---
+++ controllers/controller.template.php
@@ -0,0 +1,20 @@
+<?php
+
+class TemplateController
+{
+  //MAIN VIEW TEMPLATE
+  public function index()
+  {
+    include 'views/template.php';
+  }
+
+  //main route url
+  static public function path()
+  {
+    if (!empty($_SERVER["HTPPS"]) && ('on' == $_SERVER["HTTPS"])) {
+      return "https://" . $_SERVER['SERVER_NAME'] . "/portafolio/";
+    } else {
+      return "http://" . $_SERVER['SERVER_NAME'] . "/portafolio/";
+    }
+  }
+}
